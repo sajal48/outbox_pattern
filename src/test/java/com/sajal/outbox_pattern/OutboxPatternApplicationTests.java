d8df8fa6364f57497d560d34bb3a93a732e79ef2
Sajal <newtonh48@gmail.com>
2025-05-10 20:00:13 +0600

    git init

---
+++ src/test/java/com/sajal/outbox_pattern/OutboxPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajal.outbox_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutboxPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
